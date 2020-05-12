d5d4307390c040a5b00ae0fe8ed209fc769f091b
Michael Hallett <m.hallett@gmail.com>
2020-05-12 17:26:38 +0000

    Added session save of short URLs and added a redirect to the original URL when clicking on the newly created shortURL

--- express_server.js
+++ express_server.js
@@ -38,10 +38,24 @@ app.get("/urls", (req, res) => {
   res.render("urls_index", templateVars);
 });
 
-app.post("/urls", (req, res) => {
-  console.log(req.body);
-  res.send(`http//:www.TinyApp.com/${generateRandomString()}`);
+app.get("/u/:shortURL", (req, res) => {
+  const longURL = urlDatabase[req.params.shortURL];
+  // console.log(req.body)
+  // console.log(urlDatabase);
+  res.redirect(longURL);
 });
+
+app.post("/urls", (req, res) => {
+  let shortURL = generateRandomString()
+  urlDatabase[shortURL] = req.body.longURL;
+  res.redirect(`/urls/${shortURL}`);
+});
+
+
+app.get(("/urls/:shortURL", (req, res) => {
+
+  res.render("urls_show", templateVars);
+}));
 
 app.get("/urls/new", (req,res) =>{
   res.render("urls_new");
