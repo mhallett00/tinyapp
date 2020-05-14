15f2ae15d68b186185215ae3d682d4c915df43dc
Michael Hallett <m.hallett@gmail.com>
2020-05-14 20:58:23 +0000

    linting

--- helpers.js
+++ helpers.js
@@ -1,41 +1,41 @@
 
-// Cross checks the users object by email and returns the randonly generated user ID key if the email already exists 
-function getUserByEmail(users, email) {
+// Cross checks the users object by email and returns the randonly generated user ID key if the email already exists
+const getUserByEmail = (users, email) => {
   for (let user in users) {
     if (users[user].email === email) {
       
       return users[user];
-    } 
+    }
   }
   return undefined;
 };
 
 // Returns a 6 digit "random" string
-function generateRandomString() {
+const generateRandomString = () => {
   const alphaNumString = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
   let randomString = '';
 
-    for (i = 0; i < 6; i++) {
-      randomString += alphaNumString[Math.floor(Math.random() * alphaNumString.length)]
-    } 
+  for (let i = 0; i < 6; i++) {
+    randomString += alphaNumString[Math.floor(Math.random() * alphaNumString.length)];
+  }
 
   return randomString;
 
-  };
+};
 
-  // Returns a new object containing all the urlsDatabase entries that belong to the user
-  function urlsByUser(urls, user) {
-    const userURLS = {};
+// Returns a new object containing all the urlsDatabase entries that belong to the user
+const urlsByUser = (urls, user) => {
+  const userURLS = {};
   
-    for (url in urls) {
-      if (urls[url].userID === user) {
-       userURLS[url] = urls[url]
-      }
+  for (let url in urls) {
+    if (urls[url].userID === user) {
+      userURLS[url] = urls[url];
     }
+  }
 
-    return userURLS;
+  return userURLS;
 
-  };
+};
 
 
 module.exports = {
