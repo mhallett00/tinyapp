0a0feac129858486eb3c6acf00a901c31e09effb
Michael Hallett <m.hallett@gmail.com>
2020-05-14 20:58:45 +0000

    linting

--- test/helpersTest.js
+++ test/helpersTest.js
@@ -4,27 +4,26 @@ const { getUserByEmail } = require('../helpers.js');
 
 const testUsers = {
   "userRandomID": {
-    id: "userRandomID", 
-    email: "user@example.com", 
+    id: "userRandomID",
+    email: "user@example.com",
     password: "purple-monkey-dinosaur"
   },
   "user2RandomID": {
-    id: "user2RandomID", 
-    email: "user2@example.com", 
+    id: "user2RandomID",
+    email: "user2@example.com",
     password: "dishwasher-funk"
   }
 };
 
 describe('getUserByEmail', function() {
   it('should return a user with valid email', function() {
-    const user = getUserByEmail(testUsers, "user@example.com")
+    const user = getUserByEmail(testUsers, "user@example.com");
     const expectedOutput = "userRandomID";
-    assert.equal("userRandomID", user.id)
+    assert.equal(expectedOutput, user.id);
   });
 
   it('should return undefined if email is not in database', function() {
-    const user = getUserByEmail(testUsers, "hello@example.com")
-    const expectedOutput = "userRandomID";
+    const user = getUserByEmail(testUsers, "hello@example.com");
     assert.equal(undefined, user);
   });
 });
